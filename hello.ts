3605d597e2397a15882a78ebb4a3c09bd73e2734
cherish <cherryvuppala@gmail.com>
2024-09-23 19:04:40 +0530

    add basic commands and types to codebase

---
+++ hello.ts
@@ -0,0 +1,21 @@
+// ts basics
+let commandToCompileTypeScriptFile = "tsc fileName.ts";
+let commandToRunATypeScriptFile = "node fileName.ts";
+
+console.log(commandToCompileTypeScriptFile);
+console.log(commandToRunATypeScriptFile);
+
+// Types in TypeScript
+let nameOfPerson = "Cherish";
+let age = 20;
+let isEligibleToVote = true;
+let currentSalary = 22877.52;
+let company = "TCS";
+
+
+console.log(nameOfPerson);
+console.log(age);
+console.log(isEligibleToVote);
+console.log(currentSalary);
+console.log(company);
+
